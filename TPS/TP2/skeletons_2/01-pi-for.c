b83ee4351c6acc20616d68f5b018701b34b64da5
maximeabade <maximeabade>
2024-02-26 16:53:19 +0100

    prout

--- TPS/TP2/skeletons_2/01-pi-for.c
+++ TPS/TP2/skeletons_2/01-pi-for.c
@@ -1,35 +1,44 @@
-# include <omp.h>
-# include <stdio.h>
-# include <stdlib.h>
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main () { 
+int main() {
+  int n;
+  float N;
+  float pi, deltaX;
+  double start_time, run_time;
+  N = 100.0;
+  n = 100;
+  deltaX = 1 / N ;
+  pi = 0.0;
 
- int N, deltaX;
- float pi;
- double start_time, run_time;
+  omp_set_num_threads(4);
 
- N = 10;
- deltaX = 1/N;
+  // Start measuring the execution time here
+  start_time = omp_get_wtime();
 
- pi = 0.0;
+  #pragma omp parallel
+  {
+    float local_pi = 0.0;
 
- omp_set_num_threads(4);
+    #pragma omp for
+    for (int i = 0; i < n; i++) {
+      double x = (i + 0.5) * deltaX;
+      double term = 4.0 / (1.0 + x * x);
 
- //Start measuring the execution time here 
- start_time = omp_get_wtime();
+      local_pi += term * deltaX;
+    }
 
- #pragma omp parallel 
- {
-  #pragma omp for
-   for (int i=0;i<N;i++){
-    pi += deltaX * (4 / (1 + (i*deltaX + deltaX/2.0) * (i*deltaX + deltaX/2.0)));
-   }
- }
+    #pragma omp critical
+    {
+      pi += local_pi;
+    }
+  }
 
- //Stop measuring the execution time here 
- run_time =  omp_get_wtime() - start_time;
-	
- printf("\nPi = %f.\nTime to complete pi calculation: %lf\n",pi, run_time);
+  // Stop measuring the execution time here
+  run_time = omp_get_wtime() - start_time;
 
- return 0;
+  printf("\nPi = %f.\nTime to complete pi calculation: %lf\n", pi, run_time);
+
+  return 0;
 }
