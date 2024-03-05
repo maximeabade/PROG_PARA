8d5c1dd740a393bfc6a019bc2c51eb4add7ea485
maximeabade <maximeabade@gmail.com>
2024-03-05 10:32:34 +0100

    s

--- TPS/TP4/S04-MPI_1/skeletons/01-txrx.c
+++ TPS/TP4/S04-MPI_1/skeletons/01-txrx.c
@@ -1,35 +1,35 @@
-# include <mpi.h>
-# include <stdio.h>
+#include <mpi.h>
+#include <stdio.h>
 
-# define MPI_TAG_VALUE 123456
-	
-void run( const int myid) {
-   if ( myid == 0) {
+#define MPI_TAG_VALUE 123456
+
+void run(const int myid, const int numprocs) {
+   if (myid == 0) {
       int valtx;
       valtx = 3785;
 
-      MPI_Send(&valtx ,1, MPI_INT ,1, MPI_TAG_VALUE, MPI_COMM_WORLD);
-      valtx++;
-      MPI_Send(&valtx ,1, MPI_INT ,2, MPI_TAG_VALUE, MPI_COMM_WORLD);
-      valtx++;
-      MPI_Send(&valtx ,1, MPI_INT ,3, MPI_TAG_VALUE, MPI_COMM_WORLD);
-   }
-   else { 
+      for (int dest = 1; dest < numprocs; dest++) {
+         MPI_Send(&valtx, 1, MPI_INT, dest, MPI_TAG_VALUE, MPI_COMM_WORLD);
+         valtx++;
+      }
+   } else {
       int valrx;
       MPI_Status status;
-      MPI_Recv(&valrx ,1, MPI_INT,0, MPI_TAG_VALUE, MPI_COMM_WORLD, &status);
-      printf("proc %d received the value %d\n",myid,valrx);
+      MPI_Recv(&valrx, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+      printf("proc %d received the value %d\n", myid, valrx);
    }
 }
 
-int main(int argc , char *argv []) {
-   int myid;
+int main(int argc, char *argv[]) {
+   int myid, numprocs;
+   
    MPI_Init(&argc, &argv);
-  
+   
    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
-   run(myid);
+   MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+   
+   run(myid, numprocs);
 
    MPI_Finalize();
    return 0;
 }
-
